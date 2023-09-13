c487e4ca833e2b1190e89e4e7673dd89b3d64820
mansourmohamed2608 <mansour26082001@gmail.com>
2023-09-13 11:33:31 +0300

    Add project files.

---
+++ WebApp/Data/Entities/Department.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Data.Entities
+{
+    public class Department
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
